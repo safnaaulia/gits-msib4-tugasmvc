9834e0b8cec92b9666b5929ab4ba3a5750170a03
safnaaulia <safnaulia15@gmail.com>
2023-03-08 17:53:07 +0700

    perubahan awal

---
+++ database/migrations/2023_03_08_075301_create_mahasiswas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim');
+                $table->string('nama');
+                $table->char('jenis_kelamin', 1);
+                $table->string('alamat');
+                $table->integer('tahun_angkatan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
